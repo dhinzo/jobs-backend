58065a09d76a797f2ac582782cfda395501953a8
Devin Hinzo <devin.hinzo@gmail.com>
2020-12-08 16:28:55 -0500

    fixed database connection

--- models.py
+++ models.py
@@ -9,7 +9,7 @@ from playhouse.db_url import connect
 if 'ON_HEROKU' in os.environ:
     DATABASE = connect(os.environ.get('DATABASE_URL'))
 else:
-    DATABASE = SqliteDatabase('dogs.sqlite')
+    DATABASE = SqliteDatabase('jobs.sqlite')
 
 
 class User(UserMixin, Model):
